1103cf61265a13dd33226be69bfe53f388e235c6
Bernat Brunet <bernat@nan-tic.com>
2023-06-03 18:08:18 +0200

    Add an Intrastat update wizard, to allow update the intrastat move lines values in case something will need be modified.

--- stock.py
+++ stock.py
@@ -1,7 +1,8 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-from trytond.model import fields
+from trytond.model import fields, ModelView
 from trytond.pool import Pool, PoolMeta
+from trytond.wizard import Button, StateTransition, StateView, Wizard
 
 
 class Move(metaclass=PoolMeta):
@@ -49,7 +50,8 @@ class Move(metaclass=PoolMeta):
             self.intrastat_tariff_code = self.product.get_tariff_code(
                 self._intrastat_tariff_code_pattern_wo_country())
 
-        if self.shipment and self.shipment.intrastat_transport:
+        if (not self.intrastat_transport
+                and self.shipment and self.shipment.intrastat_transport):
             self.intrastat_transport = self.shipment.intrastat_transport
 
     def _intrastat_tariff_code_pattern_wo_country(self):
@@ -86,16 +88,17 @@ class Move(metaclass=PoolMeta):
     # TODO: Intrastat, check how VAT is get
     #o.shipment and hasattr(o.shipment, 'delivery_address') and o.shipment.delivery_address.party and o.shipment.delivery_address.party.tax_identifier and o.shipment.delivery_address.party.tax_identifier.code or '(Vacío)'
 
+    @classmethod
+    def update_intrastat_values(cls, moves):
+        pool = Pool()
+        Warning = pool.get('res.user.warning')
+        InvoiceLine = pool.get('account.invoice.line')
 
-class Move_Incoterm(metaclass=PoolMeta):
-    __name__ = 'stock.move'
+        for move in moves:
+            move._set_intrastat()
 
-    def _set_intrastat(self):
-        super()._set_intrastat()
-
-        if (not self.intrastat_transport
-                and self.shipment.intrastat_transport):
-            self.intrastat_transport = self.shipment.intrastat_transport
+        lines = (l for m in moves for l in m.invoice_lines)
+        InvoiceLine.update_intrastat_move_line_amount(lines)
 
 
 class ShipmentMixin:
@@ -124,3 +127,42 @@ class ShipmentOut(ShipmentMixin, metaclass=PoolMeta):
         except AttributeError:
             pass
         self.intrastat_transport = self.carrier.intrastat_transport
+
+
+class IntrastatUpdateStart(ModelView):
+    "Intrastat Update Start"
+    __name__ = 'account.stock.eu.intrastat.update.start'
+
+    period = fields.Many2One('account.period', 'Period', required=True,
+        domain=[
+            ('type', '=', 'standard'),
+            ])
+
+
+class IntrastatUpdate(Wizard):
+    "Intrastat Update"
+    __name__ = 'account.stock.eu.intrastat.update'
+    start = StateView(
+        'account.stock.eu.intrastat.update.start',
+        'account_stock_eu_es.intrastat_update_start_view_form', [
+            Button("Cancel", 'end', 'tryton-close'),
+            Button("Update", 'update', 'tryton-ok'),
+            ])
+    update = StateTransition()
+
+    def transition_update(self):
+        pool = Pool()
+        Line = pool.get('stock.move')
+
+        start_date = self.start.period.start_date
+        end_date = self.start.period.end_date
+        moves = Line.search([
+                ['OR',
+                    ('shipment', 'ilike', 'stock.shipment.in,%'),
+                    ('shipment', 'ilike', 'stock.shipment.out,%')],
+                ('state', '=', 'done'),
+                ('effective_date', '>=', start_date),
+                ('effective_date', '<=', end_date),
+                ])
+        Line.update_intrastat_values(moves)
+        return 'end'
