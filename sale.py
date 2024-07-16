43ef5dfe20744b509039ecffc2e3bf2a77cacabf
Bernat Brunet <bernat@nan-tic.com>
2024-07-16 11:05:53 +0200

    Only check if sale nit Incoterm if the sale is an Intrastat.

--- sale.py
+++ sale.py
@@ -1,6 +1,32 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
+from trytond.model import fields
+
+
+class Sale(metaclass=PoolMeta):
+    __name__ = 'sale.sale'
+
+    @property
+    @fields.depends('company', 'warehouse', 'shipment_address', 'sale_date')
+    def _incoterm_required(self):
+        pool = Pool()
+        Date = pool.get('ir.date')
+
+        today = Date.today()
+        if self.company and self.company.incoterms:
+            if (self.warehouse and self.warehouse.address
+                    and self.warehouse.address.country
+                    and self.warehouse.address.country.in_intrastat(
+                        date=self.sale_date or today)
+                    and self.shipment_address
+                    and self.shipment_address.country
+                    and self.shipment_address.country.in_intrastat(
+                        date=self.sale_date or today)):
+                return (
+                    self.warehouse.address.country
+                    != self.shipment_address.country)
+        return False
 
 
 class SaleLine(metaclass=PoolMeta):
