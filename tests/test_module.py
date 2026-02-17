de8e7f8ac9e4ac2906ccc6c68854414e1e3c76c7
Raimon Esteve <raimonesteve@gmail.com>
2026-02-17 08:05:42 +0100

    Add invoice_lines in move list view and FIX tests #184832

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,12 +1,26 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-from trytond.tests.test_tryton import ModuleTestCase
+from trytond.pool import Pool
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 
 
 class AccountStockEuTestCase(ModuleTestCase):
     "Test Account Stock Eu module"
-    module = 'account_stock_eu'
-    extras = ['carrier', 'incoterm']
+    module = 'account_stock_eu_es'
+    extras = ['carrier', 'incoterm', 'sale_shipment_cost']
+
+    @with_transaction()
+    def test_move_list_shipment_has_invoice_lines(self):
+        'Stock move list shipment view includes invoice_lines'
+        pool = Pool()
+        ModelData = pool.get('ir.model.data')
+        Move = pool.get('stock.move')
+
+        view_id = ModelData.get_id('stock', 'move_view_list_shipment')
+        view = Move.fields_view_get(view_id=view_id, view_type='tree')
+        arch = view['arch']
+        self.assertIn('name="invoice_lines"', arch)
+        self.assertIn('tree_invisible="1"', arch)
 
 
 del ModuleTestCase
